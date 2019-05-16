0dc3bfb9ebfea166a7c042ee8c3a12910fc0f5d0
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-16 23:36:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,9 @@
 def turn_count(board)
   counter = 0 
-  board.each do |space|
-    
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1 
+    end   
   end 
+  return counter
 end 
